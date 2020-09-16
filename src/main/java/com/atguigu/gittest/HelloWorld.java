6dd52fc99b1dd596506aaa054211cf07aff8e2d9
ZW <test@123.com0>
2020-09-16 13:50:49 +0800

    Git Test

---
+++ src/main/java/com/atguigu/gittest/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.atguigu.gittest;
+/**
+ * @author Wei Zhang
+ * @create 2020-09-16 13:31
+ */
+
+/**
+ * @author
+ * @description
+ * @date 2020/9/16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Welcome to World");
+
+        System.out.println("nihao");
+
+        System.out.println("thank you");
+    }
+}
