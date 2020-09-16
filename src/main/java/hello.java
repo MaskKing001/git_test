cfa80efb136e17545bd29dee1e07d01e65fda8d4
ZW <test@123.com0>
2020-09-16 14:59:18 +0800

    test git push

---
+++ src/main/java/hello.java
@@ -0,0 +1,15 @@
+/**
+ * @author Wei Zhang
+ * @create 2020-09-16 14:57
+ */
+
+/**
+ * @author
+ * @description
+ * @date 2020/9/16
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("sout hello world");
+    }
+}
