4e90ec53de312c748e848f652ea800ede541a881
ZW <test@123.com0>
2020-09-18 08:48:18 +0800

    Merge branch 'master' of https://github.com/MaskKing001/git_test
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -11,7 +11,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("sout hello world");
-        System.out.println("wawawa");
 
         System.out.println("third time");
     }
